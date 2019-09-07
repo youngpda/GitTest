e48a92dd98ac62233da9ee0428b0bdb697be45e7
younpgda <youngpda@naver.com>
2019-09-07 23:00:39 +0900

    //what is you name

--- src/com/gittest/GitTestView.java
+++ src/com/gittest/GitTestView.java
@@ -52,7 +52,8 @@ public class GitTestView extends ViewPart {
 		
 		// Provide the input to the ContentProvider
 		viewer.setInput(createInitialDataModel());
-		//또다른 주석
+		//hi my name is babo
+		//what is you name
 	}
 
 
