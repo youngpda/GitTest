bf559dae66cb016a1b3706b0cd086b2e608bb887
younpgda <youngpda@naver.com>
2019-09-07 20:59:59 +0900

    //////

--- src/com/gittest/GitTestView.java
+++ src/com/gittest/GitTestView.java
@@ -31,6 +31,7 @@ public class GitTestView extends ViewPart {
 		@Override
 		public Image getImage(Object obj) {
 			return null; //workbench.getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
+			///
 		}
 
 	}
